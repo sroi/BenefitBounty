f4236541609bf80d82b17e3b9064bc6af97229ce
bachkarakash <bachkarakash@gmail.com>
2019-09-07 20:09:40 +0530

    header added, ui services for project-list added
    header added, ui services for project-list added

--- src/app/project/project-list/project-list.component.ts
+++ src/app/project/project-list/project-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-project-list',
@@ -7,11 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectListComponent implements OnInit {
 
-  constructor() { }
-
+  private httpService: HttpClient;
+  constructor() {
+    
+   }
+  arrJson: any=[];
+  keys: any=[];
   ngOnInit() {
+    
+  this.fetchProjects();
+   
   }
 
+  fetchProjects()
+  {
+    this.httpService.get('http://localhost:8080/projects').subscribe(
+      data => {
+        this.arrJson = data;
+        for(const key in this.arrJson[0])
+        {
+          if(key != null)
+          {
+            this.keys.push(key);
+          }
+        }
+      },
+      (err: HttpErrorResponse) => {
+        console.log();
+      }
+    )
+  }
+
+  
   showDetails()
   {
     document.getElementById("projectDetails").hidden = false;
