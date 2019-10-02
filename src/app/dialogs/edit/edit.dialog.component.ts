50e8cb62285e8fd78e21a168c6b1f0f7f43268a7
sroi <bachkarakash@gmail.com>
2019-10-02 15:40:27 +0000

    project list update
    project list update

--- src/app/dialogs/edit/edit.dialog.component.ts
+++ src/app/dialogs/edit/edit.dialog.component.ts
@@ -2,6 +2,7 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import {Component, Inject} from '@angular/core';
 import {DataService} from '../../services/data.service';
 import {FormControl, Validators} from '@angular/forms';
+import { IMyDateModel, IMyDpOptions } from 'mydatepicker';
 
 @Component({
   selector: 'app-baza.dialog',
@@ -17,6 +18,36 @@ export class EditDialogComponent {
     Validators.required
     // Validators.email,
   ]);
+  public myDatePickerStartOptions: IMyDpOptions = {
+    // other options...
+    dateFormat: 'dd/mm/yyyy',
+  };
+
+  public myDatePickerEndOptions: IMyDpOptions = {
+    // other options...
+    dateFormat: 'dd/mm/yyyy',
+
+  };
+  onDateChanged(event: IMyDateModel) {
+    // event properties are: event.date, event.jsdate, event.formatted and event.epoc
+    this.data.startDate = event.jsdate;
+    this.restrictpastDate();
+  }
+  private getCopyOfOptions(): IMyDpOptions {
+    return JSON.parse(JSON.stringify(this.myDatePickerStartOptions));
+  }
+
+  private restrictpastDate() {
+    let date = new Date();
+
+    // Disable/enable dates from 5th backward
+    date.setDate(this.data.startDate.getDate() - 1);
+
+    let copy = this.getCopyOfOptions();
+    copy.disableUntil = { year: date.getFullYear(), month: date.getMonth() + 1, day: date.getDate() };
+    this.myDatePickerEndOptions = copy;
+
+  }
 
   getErrorMessage() {
     return this.formControl.hasError('required') ? 'Required field' :
